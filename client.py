9364838ab271c787c48d55c6e5f8bb56444a916f
door-bell <socaltdb@gmail.com>
2019-02-11 18:11:03 -0800

    basic communication template from https://www.journaldev.com/15906/python-socket-programming-server-client

--- client.py
+++ client.py
@@ -2,4 +2,25 @@
 
 import socket, threading
 
-socketString = 'localhost:8555'
+HOST = '127.0.0.1'
+PORT = 8555
+
+def client():
+    s = socket.socket()  # instantiate
+    s.connect((HOST, PORT))  # connect to the server
+
+    message = input(" -> ")  # take input
+
+    while message.lower().strip() != 'bye':
+        s.send(message.encode())  # send message
+        data = s.recv(1024).decode()  # receive response
+
+        print('Received from server: ' + data)  # show in terminal
+
+        message = input(" -> ")  # again take input
+
+    s.close()  # close the connection
+
+
+if __name__ == '__main__':
+    client()
--- server.py
+++ server.py
@@ -5,5 +5,27 @@ import socket
 HOST = '127.0.0.1'
 PORT = 8555
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def server():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    print('Starting server at {}'.format(HOST))
+    s.bind((HOST, PORT))
+
+    s.listen(5)
+
+    conn, address = s.accept()
+    print("Connection from: {}".format(address))
+
+    while True:
+        data = conn.recv(1024).decode()
+        if not data:
+            break
+        print("from connected user: {}".format(str(data)))
+        data = input (' -> ')
+        conn.send(data.encode())
+    
+    conn.close()
+
+
+if __name__ == '__main__':
+    server()
