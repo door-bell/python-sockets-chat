b0cd0ff2079c8e891772cae2333f89ee895e1014
door-bell <socaltdb@gmail.com>
2019-02-11 19:16:07 -0800

    Add nickname to client

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import socket, threading
+import socket, threading, time
 
 HOST = '127.0.0.1'
 PORT = 8555
@@ -14,16 +14,20 @@ def listen(HOST, PORT):
         data = s.recv(1024).decode()
         print('Recieved: {}'.format(data))
 
-def client():
+def client(nick='Default'):
     t1 = threading.Thread(target=listen, args=(HOST, PORT))
     t1.start()
 
     while True:
-        message = input(" -> ")  # take input
+        message = input('{} -> '.format(nick))  # take input
         s.send(message.encode())
+        time.sleep(1)
 
     s.close()  # close the connection
 
 
 if __name__ == '__main__':
-    client()
+    # HOST = str(input('Enter host: '))
+    # PORT = int(input('Enter port:'))
+    nick = str(input('Enter nickname: '))
+    client(nick=nick)
