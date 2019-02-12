662ac11d4f6345f0cd203230a029b62dfc0ab149
door-bell <socaltdb@gmail.com>
2019-02-11 19:53:25 -0800

    Get rid of global socket

--- client.py
+++ client.py
@@ -5,29 +5,30 @@ import socket, threading, time
 HOST = '127.0.0.1'
 PORT = 8555
 
-s = socket.socket()
-
-s.connect((HOST, PORT))
-
-def listen(HOST, PORT):
+def listen(sock, HOST, PORT):
     while True:
-        data = s.recv(1024).decode()
-        print('Recieved: {}'.format(data))
+        data = sock.recv(1024).decode()
+        print(data)
 
-def client(nick='Default'):
-    t1 = threading.Thread(target=listen, args=(HOST, PORT))
+def client(sock, nick='Default'):
+    t1 = threading.Thread(target=listen, args=(sock, HOST, PORT))
     t1.start()
 
     while True:
         message = input('{} -> '.format(nick))  # take input
-        s.send(message.encode())
+        sock.send(message.encode())
         time.sleep(1)
 
-    s.close()  # close the connection
+    sock.close()  # close the connection
 
 
 if __name__ == '__main__':
+    # Uncomment to allow custom host:port from user
     # HOST = str(input('Enter host: '))
     # PORT = int(input('Enter port:'))
     nick = str(input('Enter nickname: '))
-    client(nick=nick)
+
+    s = socket.socket()
+    s.connect((HOST, PORT))
+
+    client(sock=s, nick=nick)
