23f68f9c4d9c945cfcc2a588a7982ad5911950b8
door-bell <socaltdb@gmail.com>
2019-02-11 18:31:26 -0800

    attempt threads in server.py

--- server.py
+++ server.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
-import socket
+import socket, threading
 
 HOST = '127.0.0.1'
 PORT = 8555
+
+def client_thread(sock, address):
+    while True:
+        msg = sock.recv(1024).decode()
+        if not msg:
+            break
+        print(address, " ", msg)
+        sock.send(msg)
+    sock.close()
 
 def server():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,17 +22,13 @@ def server():
 
     s.listen(5)
 
-    conn, address = s.accept()
-    print("Connection from: {}".format(address))
-
     while True:
-        data = conn.recv(1024).decode()
-        if not data:
-            break
-        print("from connected user: {}".format(str(data)))
-        data = input (' -> ')
-        conn.send(data.encode())
-    
+        conn, address = s.accept() #Accept connections from within while loop
+        print("Connection from: {}".format(address))
+
+        t1 = threading.Thread(target=client_thread, args=((conn, address)))
+        t1.start()
+
     conn.close()
 
 
