13fbf787871f7be5085604db701a486faf9c81a3
Tim Beck <socaltdb@gmail.com>
2019-02-13 17:42:41 -0800

    Attempt using stdout everywhere in order to simplify overwriting lines

--- client.py
+++ client.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
+# \033[F # Back to previous line
+# \033[K # Clear line
+
 import sys, socket, threading, time
 
 HOST = '127.0.0.1'
 PORT = 8555
 
+
 def listen(sock, HOST, PORT):
     while True:
         data = sock.recv(1024).decode()
-        print(data)
+        sys.stdout.write('\r\033[K' + data)
+        sys.stdout.flush()
+        sys.stdout.write('\n')
 
 def client(sock, nick='Default'):
     t1 = threading.Thread(target=listen, args=(sock, HOST, PORT))
@@ -16,8 +22,8 @@ def client(sock, nick='Default'):
 
     while True:
         message = input('{} > '.format(nick))  # take input
+        sys.stdout.write('\033[F\033[K')
         sock.send('{}: {}'.format(nick, message).encode())
-        time.sleep(0.5)
 
     sock.close()  # close the connection
 
